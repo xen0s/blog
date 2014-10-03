dee3d1cf5b4836346d6a07863e8b2e68b12f1d13
xen0s <xen0s@hush.com>
2014-10-02 20:20:50 -0400

    blog framework

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,37 @@
 $(function() {
  
     Parse.$ = jQuery;
- 
+
+
     // Replace this line with the one on your Quickstart Guide Page
 	Parse.initialize("DKy5dGVYjWPsThtwy7sj4zhAcDN41aNkWDXkVtTR", "8FIjhKE7u6gHEc9tdnZzbpjKsKLDAthKSVO2CX81"); 
-	
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
+
+    var Blog = Parse.Object.extend("Blog");
+
+    var Blogs = Parse.Collection.extend({
+    	model: Blog
     });
+
+    var blogs = new Blogs();
+
+    blogs.fetch({
+    	success: function(blogs) {
+		    var blogsView = new BlogsView({ collection: blogs });
+		    blogsView.render();
+		    $('.main-container').html(blogsView.el);
+	    },
+    	error: function(blogs, error) {
+    		console.log(error);
+    	}
+    });
+
+    var BlogsView =  Parse.View.extend({
+	    template: Handlebars.compile($('#blogs-tpl').html()),
+	    render: function(){
+	        var collection = { blog: this.collection.toJSON() };
+	        this.$el.html(this.template(collection));
+	    }
+	});
+
  
 });
